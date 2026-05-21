c48f9235f44af041477abb0c7c4d8e2b03d4cf99
Enrico Signoretti <esignoretti@MBA-ES-5.local>
2026-05-21 22:07:33 +0200

    feat: add compilable main.go skeleton with chi router

---
+++ cmd/s3lytics/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+var version = "0.1.0-dev"
+
+func main() {
+	port := flag.String("port", "8080", "HTTP server port")
+	dataDir := flag.String("data", defaultDataDir(), "BadgerDB data directory")
+	flag.Parse()
+
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	fmt.Printf("S3lytics v%s starting on :%s\n", version, *port)
+	fmt.Printf("Data directory: %s\n", *dataDir)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprint(w, "<h1>S3lytics</h1><p>Scanning in progress...</p>")
+	})
+
+	srv := &http.Server{
+		Addr:    ":" + *port,
+		Handler: r,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("HTTP server listening on :%s", *port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	sig := <-quit
+	log.Printf("received signal %v, shutting down", sig)
+	srv.Close()
+}
+
+func defaultDataDir() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "./s3lytics-data"
+	}
+	return home + "/.s3lytics/data"
+}
