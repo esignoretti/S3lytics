7d24badb6b6a70827c1e9fb9083cf7087439d1b8
Enrico Signoretti <esignoretti@MBA-ES-5.local>
2026-05-21 22:22:40 +0200

    feat: add ListBuckets and ListObjectsPage with pagination

---
+++ internal/s3/list.go
@@ -0,0 +1,60 @@
+package s3
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func (c *CubbitS3Client) ListBuckets(ctx context.Context) ([]BucketInfo, error) {
+	resp, err := c.client.ListBuckets(ctx, &s3.ListBucketsInput{})
+	if err != nil {
+		return nil, fmt.Errorf("list buckets: %w", err)
+	}
+
+	buckets := make([]BucketInfo, 0, len(resp.Buckets))
+	for _, b := range resp.Buckets {
+		buckets = append(buckets, BucketInfo{
+			Name:         *b.Name,
+			CreationDate: *b.CreationDate,
+		})
+	}
+	return buckets, nil
+}
+
+func (c *CubbitS3Client) ListObjectsPage(ctx context.Context, bucket string, continuationToken *string) (*ListResult, error) {
+	input := &s3.ListObjectsV2Input{
+		Bucket: &bucket,
+	}
+	if continuationToken != nil {
+		input.ContinuationToken = continuationToken
+	}
+
+	resp, err := c.client.ListObjectsV2(ctx, input)
+	if err != nil {
+		return nil, fmt.Errorf("list objects v2: %w", err)
+	}
+
+	objects := make([]ObjectInfo, 0, len(resp.Contents))
+	for _, obj := range resp.Contents {
+		info := ObjectInfo{
+			Key:          *obj.Key,
+			Size:         *obj.Size,
+			LastModified: *obj.LastModified,
+		}
+		if obj.ETag != nil {
+			info.ETag = *obj.ETag
+		}
+		if obj.StorageClass != "" {
+			info.StorageClass = string(obj.StorageClass)
+		}
+		objects = append(objects, info)
+	}
+
+	return &ListResult{
+		Objects:           objects,
+		ContinuationToken: resp.NextContinuationToken,
+		IsTruncated:       *resp.IsTruncated,
+	}, nil
+}
