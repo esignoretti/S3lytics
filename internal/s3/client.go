91094607f55feb1ad4aef1edfed1de740d4aab18
Enrico Signoretti <esignoretti@MBA-ES-5.local>
2026-05-21 22:22:17 +0200

    feat: add S3Client interface and CubbitS3Client constructor

---
+++ internal/s3/client.go
@@ -0,0 +1,75 @@
+package s3
+
+import (
+	"context"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+type ObjectInfo struct {
+	Key          string
+	ETag         string
+	Size         int64
+	LastModified time.Time
+	StorageClass string
+}
+
+type ListResult struct {
+	Objects           []ObjectInfo
+	ContinuationToken *string
+	IsTruncated       bool
+}
+
+type BucketInfo struct {
+	Name         string
+	CreationDate time.Time
+}
+
+type S3Client interface {
+	ListBuckets(ctx context.Context) ([]BucketInfo, error)
+	ListObjectsPage(ctx context.Context, bucket string, continuationToken *string) (*ListResult, error)
+	HeadObject(ctx context.Context, bucket, key string) (*ObjectInfo, error)
+	ListMultipartUploads(ctx context.Context, bucket string) ([]types.MultipartUpload, error)
+	GetBucketPolicy(ctx context.Context, bucket string) (string, error)
+	GetBucketAcl(ctx context.Context, bucket string) ([]types.Grant, error)
+	GetPublicAccessBlock(ctx context.Context, bucket string) (*types.PublicAccessBlockConfiguration, error)
+	GetBucketEncryption(ctx context.Context, bucket string) (*types.ServerSideEncryptionConfiguration, error)
+	ListObjectVersions(ctx context.Context, bucket string) ([]types.ObjectVersion, []types.DeleteMarkerEntry, error)
+	GetObject(ctx context.Context, bucket, key string, rangeSpec *string) ([]byte, error)
+}
+
+type CubbitS3Client struct {
+	client *s3.Client
+}
+
+func NewCubbitS3Client(endpoint, region, accessKey, secretKey string) (*CubbitS3Client, error) {
+	customResolver := aws.EndpointResolverWithOptionsFunc(
+		func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+			return aws.Endpoint{
+				URL:               endpoint,
+				HostnameImmutable: true,
+				SigningRegion:     region,
+			}, nil
+		},
+	)
+
+	cfg, err := config.LoadDefaultConfig(context.Background(),
+		config.WithRegion(region),
+		config.WithEndpointResolverWithOptions(customResolver),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKey, secretKey, "")),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = true
+	})
+
+	return &CubbitS3Client{client: client}, nil
+}
