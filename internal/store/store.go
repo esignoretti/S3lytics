edfc6b81eade920d4764e4cb8788a4bba8036d8a
Enrico Signoretti <esignoretti@MBA-ES-5.local>
2026-05-21 22:16:19 +0200

    feat: add auth store methods (session, account, clear)

--- internal/store/store.go
+++ internal/store/store.go
@@ -118,6 +118,48 @@ func (s *BadgerStore) iterateKeys(ctx context.Context, prefix []byte) ([]string,
 	return keys, err
 }
 
+func (s *BadgerStore) SaveSession(ctx context.Context, data *SessionData) error {
+	return s.set(ctx, prefixAuthSession, data)
+}
+
+func (s *BadgerStore) GetSession(ctx context.Context) (*SessionData, error) {
+	data, err := s.get(ctx, prefixAuthSession)
+	if err != nil {
+		return nil, err
+	}
+	var sd SessionData
+	if err := json.Unmarshal(data, &sd); err != nil {
+		return nil, err
+	}
+	return &sd, nil
+}
+
+func (s *BadgerStore) SaveAccount(ctx context.Context, data *AccountData) error {
+	return s.set(ctx, prefixAuthAccount, data)
+}
+
+func (s *BadgerStore) GetAccount(ctx context.Context) (*AccountData, error) {
+	data, err := s.get(ctx, prefixAuthAccount)
+	if err != nil {
+		return nil, err
+	}
+	var ad AccountData
+	if err := json.Unmarshal(data, &ad); err != nil {
+		return nil, err
+	}
+	return &ad, nil
+}
+
+func (s *BadgerStore) ClearAuth(ctx context.Context) error {
+	if err := s.del(ctx, prefixAuthSession); err != nil && err != badger.ErrKeyNotFound {
+		return err
+	}
+	if err := s.del(ctx, prefixAuthAccount); err != nil && err != badger.ErrKeyNotFound {
+		return err
+	}
+	return nil
+}
+
 func (s *BadgerStore) iterateValues(ctx context.Context, prefix []byte) ([][]byte, error) {
 	var vals [][]byte
 	err := s.db.View(func(txn *badger.Txn) error {
